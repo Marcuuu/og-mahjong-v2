51f8442a40eeecf4f93a5f07babb6692607c2e4b
Marc Lim <marclim56@gmail.com>
2021-07-07 01:57:11 +0800

    test

--- app.js
+++ app.js
@@ -8,56 +8,37 @@ bot.start(ctx => {
   ctx.reply('Bot has started');
 });
 
-const dates = [];
-const days = [];
-
-const buttons = [];
-let temp = [];
-
 bot.command('play', (ctx) => {
+  const buttons = []
+  let temp = []
+
   for (let i = 0; i < 6; i++) {
-    const mod = i % 2;
-    const date = moment().add(i, 'days').format('DD MMM');
-    const day = moment().add(i, 'days').format('dddd');
+    const mod = i % 2
+    const date = moment().add(i, 'days').format('DD MMM')
+    const day = moment().add(i, 'days').format('dddd')
     temp.push({
       text: `${date}, ${day}`,
       callback_data: `day${i + 1}`,
     })
     if (mod === 1) {
-      buttons.push(temp);
-      temp = [];
+      buttons.push(temp)
+      temp = []
     }
-  }
-  ctx.telegram.sendMessage(
-    ctx.chat.id,
-    'Select a date',
-    {
-      reply_markup: {
-        inline_keyboard: buttons
-      }
-    }
-  )
+  };
+
+  ctx.telegram.sendMessage(ctx.chat.id, 'Select a date', {
+    reply_markup: {
+      inline_keyboard: buttons,
+    },
+  })
 });
 
-bot.hears('animals', ctx => {
-  console.log(ctx.from);
-  ctx.telegram.sendMessage(
-    ctx.chat.id,
-    'You like animals?',
-    {
-      reply_markup: {
-        one_time_keyboard: true,
-        keyboard: [
-          [{
-            text: 'My location',
-            request_location: true,
-            one_time_keyboard: true
-          }],
-          ['Cancel']
-        ]
-      }
-    }
-  );
+bot.on('callback_query', ctx => {
+  ctx.telegram.answerCbQuery(ctx.callbackQuery.id)
+});
+
+bot.catch(err => {
+  console.log('Encountered an error', err);
 });
 
 // bot.launch();
