33c9fc1f7f42da411ee43058ebb5b3df54d46f10
Marc Lim <marclim56@gmail.com>
2021-07-07 01:35:19 +0800

    added reply markup

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
 require('dotenv').config()
-// const { Telegraf } = require('telegraf');
 const { Composer } = require('micro-bot');
+const moment = require('moment');
 
-// const bot = new Telegraf(process.env.TOKEN);
 const bot = new Composer;
 
 bot.start(ctx => {
   ctx.reply('Bot has started');
 });
 
+// const dates = [];
+// const days = [];
+
+// for (let i = 0; i < 6; i++) {
+//   const temp = [];
+//   const date = moment().add(i, 'days').format('DD MMM');
+//   const day = moment().add(i, 'days').format('dddd');
+//   temp.push({
+//     text: `${i + 1} - ${date}, ${day}\nPlayers: ${players}`,
+//     callback_data: ''
+//   });
+//   console.log(temp)
+// }
+
 bot.command('play', (ctx) => {
-  console.log(ctx.from);
-  ctx.reply('hihi')
+  ctx.telegram.sendMessage(
+    ctx.chat.id,
+    'Select a date',
+    {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: 'hihi',
+              callback_data: 'nono'
+            }
+          ]
+        ]
+      }
+    }
+  )
 });
 
 bot.hears('animals', ctx => {
   console.log(ctx.from);
-  bot.telegram.sendMessage(
+  ctx.telegram.sendMessage(
     ctx.chat.id,
     'You like animals?',
     {
